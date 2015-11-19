92b7c8c0163444d814643b511308ec1a98b1dce0
David Song <songdavid98@gmail.com>
2015-11-19 11:37:10 -0500

    updated app

--- app.py
+++ app.py
@@ -5,11 +5,30 @@ import urllib2, google, bs4, re
 
 app = Flask(__name__)
 
+"""
+whatever, just a test or something
+"""
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
+"""
+This is the search page, where you type in what you want to search
+"""
+@app.route("/search")
+@app.route("/search/")
+def search():
+    return render_template("home.html")
+
+"""
+This is the results page
+"""
+@app.route("/results")
+@app.route("/results/")
+def results():
+    return render_template("results.html")
 
 if __name__ == "__main__":
     app.debug = True
