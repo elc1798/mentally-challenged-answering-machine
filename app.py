8e894cc21689d9a2bc575eacde0b0169cd988b03
elc1798 <subliminalmau5@gmail.com>
2015-11-22 19:27:33 -0500

    Wrote the beginning of the backend

--- app.py
+++ app.py
@@ -16,13 +16,26 @@ def search():
     Params:
         none
 
-"""
-This is the results page
-"""
-@app.route("/results")
-@app.route("/results/")
-def results():
-    return render_template("results.html")
+    Returns:
+        A rendered Jinja template (index.html) with the following parameters:
+            QUERY = query
+            RESULT = result
+    """
+    if "query" not in request.args:
+        return render_template("index.html")
+    # Run a Google search for the query
+    query = request.args["query"]
+    gsearch_res = list(google.search(query, num=10, start=0, stop=10))
+    # Get the web page and strip the tags. Note that 'wgotten' is a pun from
+    # the wget command for retrieving webpages from the command line.
+    wgotten = urllib2.urlopen(gsearch_res[0])
+    webpage = wgotten.read()
+    beautified_soup = bs4.BeautifulSoup(webpage,'html')
+    wtext = soup.get_text()
+    # Run regex parsing to get the query
+        # TODO: Write the code for the regex parsing
+    # Return the rendered Jinja template
+    return render_template("index.html", QUERY=query, RESULT=result)
 
 if __name__ == "__main__":
     app.debug = True
