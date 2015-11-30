f6744b312903149b07c989eb5bd396d3dde8ffc5
Johnny So <jso123450@gmail.com>
2015-11-30 00:06:04 -0500

    Hopefully working

--- regex.py
+++ regex.py
@@ -24,21 +24,25 @@ def parse(query,text):
     exp = "The Regular Expression used to search through text"
     stuff = ["will be a list of tuples later after using re.findall"]
         
-    if query[0:2]=="Who" or query[0:3]=="What" or query[0:2]=="Why" or query[0:2]=="How":
+    if query[0:3]=="Who" or query[0:4]=="What" or query[0:3]=="Why" or query[0:3]=="How":
         # Get the stuff between the paragraphs tags
         exp = "p>([\w\s]*)</p"
 
     # If the query is asking "Where"
-    if query[0:4] == "Where":
+    if query[0:5] == "Where":
         # Look for something along the lines of "United States"
         exp = "[A-Z][a-z]+\s+[A-Z][a-z]+"
 
     # If the query is asking "When"
-    if query[0:3] == "When":
-        # Look for something along the lines of "January 1,
-        exp = "[A-Z][a-z]+\s\d\d,\s\d\d\d\d"
+    if query[0:4] == "When":
+        # Look for something along the lines of "January 1, 2015"
+        exp = "[A-Z][a-z]+\s\d{1,2},\s\d\d\d\d"
     
+    print query
+    print exp
+    print text.encode('utf-8')
     stuff = re.findall(exp,text)
+    print stuff
 
     # Filter stuff into result
     result = []
